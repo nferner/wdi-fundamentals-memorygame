84fd8a7731b2987cf62520cea56ba0b68e7a14d1
nferner <n.e.ferner@gmail.com>
2016-08-14 19:58:28 -0400

    latest version of assignment

---
+++ main.js
@@ -0,0 +1,43 @@
+/* Confused with JS, but looking forward to more work*/
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+/*if (cardTwo !== cardFour){alert('Sorry, no match.');}
+else if (cardTwo !== cardThree){alert('Sorry, no match.');}
+else if (cardOne !== cardFour){alert('Sorry, no match.');}
+else if (cardOne !== cardThree){alert('Sorry, no match.');}
+else if (cardOne === cardTwo){alert('You found a match!');}
+else if (cardThree === cardFour) {alert('You found a match!')}*/
+document.createElement('div')
+console.log('div');
+
+var board = document.getElementByID('game-board');
+
+var createCard = document.getElementsByClassName('card') {
+	for (var i = 0; i =< card; i += 1);
+		if document.getElementsByClassName
+}
+for document.getElementsByClassName('card') 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var createBoard = function(cards) {
+for (var i = 0; i < card.length; i ++);
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.appenChild(board);
+	}
+
+var isMatch = function(cards) {
+	for 
+}
+
+function isTwoCards(){}
+
+
+
+
+
+
