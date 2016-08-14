1ef8b633ce6fb686384362f736f6093771cb9995
nferner <n.e.ferner@gmail.com>
2016-08-14 13:21:37 -0400

    Neil Commits

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,15 @@
-console.log("JS file is connected to HTML! Woo!")
+/* Testing js first time */
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+/*if (cardTwo !== cardFour){alert('Sorry, no match.');}
+else if (cardTwo !== cardThree){alert('Sorry, no match.');}
+else if (cardOne !== cardFour){alert('Sorry, no match.');}
+else if (cardOne !== cardThree){alert('Sorry, no match.');}
+else if (cardOne === cardTwo){alert('You found a match!');}
+else if (cardThree === cardFour) {alert('You found a match!')}*/
+	document.createElement('div')
+console.log('div');
+
+document.getElementByID('game-board')
